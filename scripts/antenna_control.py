42a214bcea0ef1cb442df53a4a1fab1fcb496479
k.noda <k.noda@a.phys.nagoya-u.ac.jp>
2018-09-03 16:11:49 -0300

    create antenna_control, emergency

---
+++ scripts/antenna_control.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+name = "antenna_control"
+
+# ----
+import rospy
+from std_msgs.msg import Bool
+from std_msgs.msg import String
+
+def antenna_control_mapper(status):
+    if status.data == True:
+        topic_to.publish("LOCAL")
+    else:
+        topic_to.publish("REMOTE")
+        pass
+    return
+
+
+if __name__=="__main__":
+    rospy.init_node(name)
+
+    topic_to = rospy.Publisher(
+        name = name,
+        data_class = String,
+        latch = True,
+        queue_size = 1,
+    )
+    
+    topic_from = rospy.Subscriber(
+        name = 'cpz2724_rcw0_dio?',
+        data_class = Bool,
+        callback = antenna_control_mapper,
+        queue_size = 1,
+    )
+
+    rospy.spin()
---
+++ scripts/antenna_emergency.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+name = 'antenna_emergency'
+
+# ----
+import rospy
+from std_msgs.msg import Bool
+
+def antenna_emergency_mapper(status):
+    topic_to.publish(status)
+    return
+
+
+if __name__=='__main__':
+    rospy.init_node(name)
+
+    topic_to = rospy.Publisher(
+        name = name,
+        data_class = Bool,
+        latch = True,
+        queue_size = 1,
+    )
+    
+    topic_from = rospy.Subscriber(
+        name = 'cpz2724_rcw0_dio?',
+        data_class = Bool,
+        callback = antenna_emergency_mapper,
+        queue_size = 1,
+    )
+
+    rospy.spin()
