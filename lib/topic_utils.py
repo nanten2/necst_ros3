0c5a9dca75a0b9995a6a9381486f453ed82ebb92
Atsushi Nishimura <ars096@gmail.com>
2018-09-04 06:43:56 +0900

    create topic_utils module

---
+++ lib/topic_utils.py
@@ -0,0 +1,31 @@
+
+import rospy
+import time
+
+
+def recv(topic_name, data_class):
+    recv_msg = None
+    received = False
+    
+    def callback(msg):
+        recv_msg = msg
+        received = True
+        return
+    
+    sub = rospy.Subscriber(
+        name = topic_name,
+        data_class = data_class,
+        callback = callback,
+        queue_size = 1,
+    )
+    
+    while True:
+        if reveived == True:
+            break
+
+        time.sleep(0.001)
+        continue
+
+    return recv_msg
+
+
