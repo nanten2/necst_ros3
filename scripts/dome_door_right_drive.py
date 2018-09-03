c595c85611022f9deace2edd27c23b4acf77e220
Atsushi Nishimura <ars096@gmail.com>
2018-09-04 05:30:13 +0900

    create dome_door current status nodes

---
+++ scripts/dome_door_right_drive.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+
+name = 'dome_door_right_drive'
+
+# ----
+import rospy
+import std_msgs.msg
+
+def dome_door_right_drive_mapper(status):
+    if status.data == True:
+        topic_to.publish('DRIVE')
+    else:
+        topic_to.publish('OFF')
+        pass
+    return
+
+
+if __name__=='__main__':
+    rospy.init_node(name)
+
+    topic_to = rospy.Publisher(
+        name = name,
+        data_class = std_msgs.msg.String,
+        latch = True,
+        queue_size = 1,
+    )
+    
+    topic_from = rospy.Subscriber(
+        name = 'cpz2724_rsw2_dio2',
+        data_class = std_msgs.msg.Bool,
+        callback = dome_door_right_drive_mapper,
+        queue_size = 1,
+    )
+
+    rospy.spin()
