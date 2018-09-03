26e851202f750993330138268d6d1bf2edb409e7
Atsushi Nishimura <ars096@gmail.com>
2018-09-04 02:58:04 +0900

    create simulator versionof dome_emergency

---
+++ scripts/dome_emergency_sim.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+name = 'dome_emergency'
+
+# ----
+import rospy
+import std_msgs.msg
+
+def dome_emergency_mapper(status):
+    topic_to.publish(status)
+    return
+
+
+if __name__=='__main__':
+    rospy.init_node(name)
+
+    topic_to = rospy.Publisher(
+        name = name,
+        data_class = std_msgs.msg.Bool,
+        latch = True,
+        queue_size = 1,
+    )
+    
+    topic_from = rospy.Subscriber(
+        name = 'dome_emergency_sim',
+        data_class = std_msgs.msg.Bool,
+        callback = dome_emergency_mapper,
+        queue_size = 1,
+    )
+
+    rospy.spin()
