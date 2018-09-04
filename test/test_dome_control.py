a35090469f9cfb9d5d78048fef5c26e733b20f61
Atsushi Nishimura <ars096@gmail.com>
2018-09-04 23:20:15 +0900

    fix typo

--- test/test_dome_control.py
+++ test/test_dome_control.py
@@ -46,7 +46,7 @@ class TestDomeControl(unittest.TestCase):
         
     def recv(self):
         start = time.time()
-        while True;:
+        while True:
             if self.received == True:
                 break
             if time.time() - start > self.timeout:
