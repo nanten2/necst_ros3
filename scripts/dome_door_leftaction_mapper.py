a64f765658e3eedcd52347fabd551ca770dc4098
Atsushi Nishimura <ars096@gmail.com>
2018-09-07 11:41:11 +0900

    create dome_door_leftaction_mapper test

--- scripts/dome_door_leftaction_mapper.py
+++ scripts/dome_door_leftaction_mapper.py
@@ -1,16 +1,16 @@
 #! /usr/bin/env python3
 
-name = 'dome_door_leftdrive_mapper'
+name = 'dome_door_leftaction_mapper'
 
 # ----
 import rospy
 import std_msgs.msg
 
-def dome_door_left_drive_mapper(status):
+def dome_door_leftaction_mapper(status):
     if status.data == True:
-        topic_to.publish('DRIVE')
+        topic_to.publish('MOVE')
     else:
-        topic_to.publish('OFF')
+        topic_to.publish('STOP')
         pass
     return
 
@@ -26,7 +26,7 @@ if __name__=='__main__':
     )
     
     topic_from = rospy.Subscriber(
-        name = 'cpz2724_rsw2_di5',
+        name = 'cpz2724_rsw2_di05',
         data_class = std_msgs.msg.Bool,
         callback = dome_door_left_drive_mapper,
         queue_size = 1,
