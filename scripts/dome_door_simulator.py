eb524c89a7b09b2ce479594507f9c7be723dbb60
Atsushi Nishimura <ars096@gmail.com>
2018-09-09 23:56:22 +0900

    change to use relay node

--- scripts/dome_door_simulator.py
+++ scripts/dome_door_simulator.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-name = 'dome_door_cmd_sim'
+name = 'door_simulator'
 
 # ----
 import time
@@ -11,50 +11,87 @@ import std_msgs.msg
 import topic_utils
 
 
-class dome_door_cmd_sim(object):
+class dome_door_im(object):
+    p = {}
+    
     def __init__(self, travel_time_left, travel_time_right):
         self.sim = dome_door_simulator(travel_time_left, travel_time_right)
         
-        self.topic_from1 = rospy.Subscriber(
-            name = 'dome_door_cmd2',
+        self.do1 = rospy.Subscriber(
+            name = '/cpz2724_rsw2/do05',
             data_class = std_msgs.msg.String,
-            callback = self.update_cmd,
+            callback = self.callback,
+            callback_args = 'do1',
             queue_size = 1,
         )
         
-        self.pub_left = rospy.Publisher(
-            name = 'dome_door_leftposition',
+        self.do2 = rospy.Subscriber(
+            name = '/cpz2724_rsw2/do06',
             data_class = std_msgs.msg.String,
+            callback = self.callback,
+            callback_args = 'do2',
+            queue_size = 1,
+        )
+        
+        self.pub_left1 = rospy.Publisher(
+            name = '/cpz2724_rsw2/di06',
+            data_class = std_msgs.msg.Bool,
             latch = True,
             queue_size = 1,
         )
         
-        self.pub_right = rospy.Publisher(
-            name = 'dome_door_rightposition',
-            data_class = std_msgs.msg.String,
+        self.pub_left2 = rospy.Publisher(
+            name = '/cpz2724_rsw2/di07',
+            data_class = std_msgs.msg.Bool,
             latch = True,
             queue_size = 1,
         )
-
+        
+        self.pub_right1 = rospy.Publisher(
+            name = '/cpz2724_rsw2/di03',
+            data_class = std_msgs.msg.Bool,
+            latch = True,
+            queue_size = 1,
+        )
+        
+        self.pub_right2 = rospy.Publisher(
+            name = '/cpz2724_rsw2/di04',
+            data_class = std_msgs.msg.Bool,
+            latch = True,
+            queue_size = 1,
+        )
+        
         self.pub_left_act = rospy.Publisher(
-            name = 'dome_door_leftaction',
-            data_class = std_msgs.msg.String,
+            name = '/cpz2724_rsw2/di05',
+            data_class = std_msgs.msg.Bool,
             latch = True,
             queue_size = 1,
         )
         
         self.pub_right_act = rospy.Publisher(
-            name = 'dome_door_rightaction',
-            data_class = std_msgs.msg.String,
+            name = '/cpz2724_rsw2/di02',
+            data_class = std_msgs.msg.Bool,
             latch = True,
             queue_size = 1,
         )
         pass
-    
-    def update_cmd(self, msg):
-        self.sim.set_command(msg.data)
+
+    def callback(self, msg, argname):
+        self.p[argname] = msg.data
+        self.callback2()
         return
-    
+
+    def callback2(self):
+        if self.p['do1'] == True and self.p['do2'] == True:
+            self.sim.set_command('OPEN')
+        elif self.p['do1'] == False and self.p['do2'] == True:
+            self.sim.set_command('CLOSE')
+        elif self.p['do1'] == False and self.p['do2'] == False:
+            self.sim.set_command('STOP')
+        else:
+            pass
+        return
+            
     def publish_status(self):
         pos_left_last = ''
         pos_right_last = ''
@@ -64,25 +101,51 @@ class dome_door_cmd_sim(object):
         while not rospy.is_shutdown():
             pos_left = self.sim.current_left
             if pos_left != pos_left_last:
-                self.pub_left.publish(pos_left)
+                if pos_left == 'OPEN':
+                    self.pub_left1.publish(True)
+                    self.pub_left2.publish(True)
+                elif pos_left == 'CLOSE':
+                    self.pub_left1.publish(False)
+                    self.pub_left2.publish(True)
+                elif pos_left == 'TRANSIT':
+                    self.pub_left1.publish(False)
+                    self.pub_left2.publish(False)
+                    pass
                 pos_left_last = pos_left
                 pass
             
             pos_right = self.sim.current_right
             if pos_right != pos_right_last:
-                self.pub_right.publish(pos_right)
+                if pos_right == 'OPEN':
+                    self.pub_right1.publish(True)
+                    self.pub_right2.publish(True)
+                elif pos_right == 'CLOSE':
+                    self.pub_right1.publish(False)
+                    self.pub_right2.publish(True)
+                elif pos_right == 'TRANSIT':
+                    self.pub_right1.publish(False)
+                    self.pub_right2.publish(False)
+                    pass
                 pos_right_last = pos_right
                 pass
 
             act_left = self.sim.current_left_action
             if act_left != act_left_last:
-                self.pub_left_act.publish(act_left)
+                if act_left == 'MOVE':
+                    self.pub_left_act.publish(True)
+                elif act_left == 'STOP':
+                    self.pub_left_act.publish(False)
+                    pass
                 act_left_last = act_left
                 pass
             
             act_right = self.sim.current_right_action
             if act_right != act_right_last:
-                self.pub_right_act.publish(act_right)
+                if act_right == 'MOVE':
+                    self.pub_right_act.publish(True)
+                elif act_right == 'STOP':
+                    self.pub_right_act.publish(False)
+                    pass
                 act_right_last = act_right
                 pass
             
@@ -183,7 +246,7 @@ if __name__=='__main__':
     travel_time_left = rospy.get_param('~travel_time_left')
     travel_time_right = rospy.get_param('~travel_time_right')
     
-    sim = dome_door_cmd_sim(travel_time_left, travel_time_right)
+    sim = dome_door_sim(travel_time_left, travel_time_right)
     pub_thread = threading.Thread(
         target = sim.publish_status,
         daemon = True,
