f9751c8d4087a66dd68b75e6c3969e7bf2329fea
kenya-noda <k.noda@a.phys.nagoya-u.ac.jp>
2018-10-12 07:54:45 +0000

    create simulator

--- devtools/topic_monitor.py
+++ devtools/topic_monitor.py
@@ -37,6 +37,25 @@ class topic_monitor(object):
         new('dome_door_rightposition', std_msgs.msg.String)
         new('dome_emergency', std_msgs.msg.Bool)
         
+        # Weather
+        # -------
+        new('/weather/B_humi', std_msgs.msg.Float32)
+        new('/weather/B_temp', std_msgs.msg.Float32)
+        new('/weather/D_humi', std_msgs.msg.Float32)
+        new('/weather/D_temp', std_msgs.msg.Float32)
+        new('/weather/cabin_temp1', std_msgs.msg.Float32)
+        new('/weather/cabin_temp2', std_msgs.msg.Float32)
+        new('/weather/dome_temp1', std_msgs.msg.Float32)
+        new('/weather/dome_temp2', std_msgs.msg.Float32)
+        new('/weather/outside1_humi', std_msgs.msg.Float32)
+        new('/weather/outside1_temp', std_msgs.msg.Float32)
+        new('/weather/outside2_humi', std_msgs.msg.Float32)
+        new('/weather/outside2_temp', std_msgs.msg.Float32)
+        new('/weather/press', std_msgs.msg.Float32)
+        new('/weather/rain', std_msgs.msg.Float32)
+        new('/weather/wind_direction', std_msgs.msg.Float32)
+        new('/weather/wind_speed', std_msgs.msg.Float32)
+
         pass
     
     def callback(self, msg, args):
