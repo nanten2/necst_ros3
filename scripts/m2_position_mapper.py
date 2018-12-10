95e469ca3fb8d4c4439c2eaefbeb417f9023522c
kenya-noda <k.noda@a.phys.nagoya-u.ac.jp>
2018-12-10 10:48:30 +0000

    fix

--- scripts/m2_position_mapper.py
+++ scripts/m2_position_mapper.py
@@ -12,6 +12,8 @@ import std_msgs.msg
 
 class m2_position_mapper(object):
 
+    puls = None
+
     def __init__(self):
         self.array = std_msgs.msg.ByteMultiArray()
 
@@ -44,46 +46,46 @@ class m2_position_mapper(object):
 
     def pub_function(self):
         puls_last = None
-        while not rospy.is_shutdonw():
+        while not rospy.is_shutdown():
             if self.puls != puls_last:
                 if self.puls >= -65535 and self.puls <= 65535:
                     #index mode
                     self.array.data = [0,0,0,1,0,0,0,0]
                     self.do1.publish(self.array)
-                    Strobe()
+                    self.Strobe()
                     #step no.
                     self.array.data = [1,1,1,1,1,1,1,1]
                     self.do1.publish(self.array)
-                    Strobe()
+                    self.Strobe()
                     #position set
                     self.array.data = [0,0,0,0,0,0,1,1]
                     self.do1.publish(self.array)
-                    Strobe()
+                    self.Strobe()
                     #direction
-                    if puls >= 0:
+                    if self.puls >= 0:
                         self.array.data = [0,0,0,0,1,0,0,0] #CW
                         self.do1.publish(self.array)
-                        Strobe()
+                        self.Strobe()
                     else:
                         self.array.data = [1,0,0,0,1,0,0,0] #CCW
                         self.do1.publish(self.array)
-                        Strobe()
+                        self.Strobe()
                     #displacement
                     self.array.data = [0,0,0,0,0,0,0,0]
                     self.do1.publish(self.array)
-                    Strobe()
+                    self.Strobe()
                     pls = struct.pack("<I", int(abs(self.puls)/256))
                     self.array.data = list(map(int, "".join([format(b, "08b")[::-1] for b in pls])))[:8]
                     self.do1.publish(self.array)
-                    Strobe()
+                    self.Strobe()
                     pls = struct.pack("<I", int(abs(self.puls)%256))
                     self.array.data = list(map(int, "".join([format(b, "08b")[::-1] for b in pls])))[:8]
                     self.do1.publish(self.array)
-                    Strobe()
+                    self.Strobe()
                     #start
                     self.array.data = [0,0,0,1,1,0,0,0]
                     self.do1.publish(self.array)
-                    Strobe()
+                    self.Strobe()
                     time.sleep((abs(self.puls) / 200 / 10.) + 1.)
                 else:
                     pass
@@ -119,67 +121,66 @@ class m2_position_mapper(object):
         #initialization?
         self.array.data = [0,0,0,1,0,0,0,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         #step no.
         self.array.data = [1,1,1,1,1,1,1,1]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         #vs set
         self.array.data = [0,1,0,0,1,0,0,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         #5(*10=50)
         self.array.data = [0,0,0,0,0,0,0,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         self.array.data = [1,0,1,0,0,0,0,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         #vr set
         self.array.data = [0,0,0,0,0,0,1,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         
         self.array.data = [0,0,0,0,0,0,0,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         self.array.data = [0,0,0,1,0,0,1,1] # MOTOR_SPEED=200
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         #su-sd set
         self.array.data = [0,0,0,0,1,0,1,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         #100(/10=10)
         self.array.data = [0,0,0,0,0,0,0,0]
         self.do1.publish(self.array)
-        dio.output_byte("OUT1_8", [0,0,0,0,0,0,0,0])
-        StrobeHOff()
+        self.StrobeHOff()
         self.array.data = [0,0,1,0,0,1,1,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         #position set
         self.array.data = [0,0,0,0,0,0,1,1]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         #cw
         self.array.data = [0,0,0,0,1,0,0,0] #CW=0x10
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         #0
         self.array.data = [0,0,0,0,0,0,0,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         self.array.data = [0,0,0,0,0,0,0,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         self.array.data = [0,0,0,0,0,0,0,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         #start
         self.array.data = [0,0,0,1,1,0,0,0]
         self.do1.publish(self.array)
-        StrobeHOff()
+        self.StrobeHOff()
         return
 
 
